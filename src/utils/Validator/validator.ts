57670cc6c5bb88121dfb7791ea2d0628a5ad97c6
msienkiewicz7 <msienkiewicz@disroot.org>
2021-06-24 20:37:59 +0200

    speced basic input validations

--- src/utils/Validator/validator.ts
+++ src/utils/Validator/validator.ts
@@ -1,5 +1,10 @@
 export function validInput(currentInput: string, nextKey: string) : boolean {
 
+    //TODO: following zeroes (eg. 00)
+
+    //TODO: following operands (e.g. 1+-)
+
+    //TODO: following decimal points (e.g. 1,,)
 
     return true
 }
