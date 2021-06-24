72fd1192de38aaac9d7915c4ca6dcfcd795aed5d
msienkiewicz7 <msienkiewicz@disroot.org>
2021-06-24 19:53:57 +0200

    added input keys

--- src/components/CalcButton/CalcButton.tsx
+++ src/components/CalcButton/CalcButton.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './CalcButton.module.css';
 
-const CalcButton = () => (
+interface Props {
+  inputKey: string
+}
+
+const CalcButton = (props: Props) => (
   <div className={styles.CalcButton} data-testid="CalcButton">
-    CalcButton Component
+    <button>{props.inputKey}</button>
   </div>
 );
 
--- src/pages/CalculatorPage/CalculatorPage.tsx
+++ src/pages/CalculatorPage/CalculatorPage.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import CalcButton from '../../components/CalcButton/CalcButton';
 import styles from './CalculatorPage.module.css';
 
-const CalculatorPage = () => (
-  <div className={styles.CalculatorPage} data-testid="CalculatorPage">
-    CalculatorPage Component
-  </div>
-);
+
+const inputKeys = [['7', '8', '9'], ['4', '5', '6'], ['1', '2', '3'], ['0', ',']]
+
+const CalculatorPage = () => {
+
+  return (
+    <div className={styles.CalculatorPage} data-testid="CalculatorPage">
+      <div className={styles.Calculator}> 
+        
+      {inputKeys.map(rowKeys => (
+        <div className={styles.KeyRow}>
+          {
+            rowKeys.map(key => (
+              <CalcButton key={key} inputKey={key} />
+            ))
+          }
+        </div>
+
+      ))}
+      </div>
+    </div>
+  )
+};
 
 export default CalculatorPage;
