0bf7f493efa191c28707455a0a145d6adabb5135
msienkiewicz7 <msienkiewicz@disroot.org>
2021-06-25 20:07:02 +0200

    describe token validation

--- src/utils/ExpressionEvaluator/expressionEvaluator.test.ts
+++ src/utils/ExpressionEvaluator/expressionEvaluator.test.ts
@@ -15,21 +15,45 @@ describe("ExpressionEvaluation", () => {
         expect(token).toHaveProperty('value', 2)
     });
 
+
+
+
+
+
+});
+
+describe("token validation", () => {
     it("creates valid token from string without operation character", () => {
         const stringToken = "2"
         const token = new Token(stringToken)
-        expect(token).toHaveProperty('operand', operations[""])
+        expect(token).toHaveProperty('operand', operations["+"])
         expect(token).toHaveProperty('value', 2)
     });
 
-    it("evaluates two tokens", () => {
+    it("evaluates two regular tokens (positive result)", () => {
+        const a = new Token('-2')
+        const b = new Token('+3')
+        const result = Token.evaluateTokens(a, b)
+
+        expect(result).toHaveProperty('operand', operations["+"])
+        expect(result).toHaveProperty('value', 1)
+    });
+
+    it("evaluates two regular tokens (negative result)", () => {
+        const a = new Token('-2')
+        const b = new Token('-3')
+        const result = Token.evaluateTokens(a, b)
+
+        expect(result).toHaveProperty('operand', operations["+"])
+        expect(result).toHaveProperty('value', -5)
+    });
+
+    it("evaluates one irregular and one regular token", () => {
         const a = new Token('2')
         const b = new Token('+3')
         const result = Token.evaluateTokens(a, b)
 
-        expect(result).toHaveProperty('operand', operations[""])
+        expect(result).toHaveProperty('operand', operations["+"])
         expect(result).toHaveProperty('value', 5)
     })
-
-
-});
+})
