a60bc1c22b31b1cfc79a1e6e53cbebf73d6b2fef
msienkiewicz7 <msienkiewicz@disroot.org>
2021-06-25 19:10:58 +0200

    implement token logic

--- src/utils/ExpressionEvaluator/evaluator.ts
+++ src/utils/ExpressionEvaluator/evaluator.ts
@@ -8,6 +8,53 @@ export function splitExpressionByLogic(expression: string) : string[] {
     return expression.match(/[+\-*/]*(\.\d+|\d+(\.\d+)?)/g) || []  
 }
 
+type Operation = {
+    key: string,
+    prio: number,
+    calculate: (a: number, b: number) => number
+}
+
+export const operations = {
+    '+': {
+        key: '+',
+        prio: 1,
+        calculate: function (a: number, b: number): number {
+            return a + b
+        }
+    } as Operation,
+    '': {
+        key: '',
+        prio: 0,
+        calculate: function (a: number, b: number): number {
+            return a
+        }
+    } as Operation
+}
+
+export function getOperandByKey(key: string) : Operation {
+    switch (key) {
+        case '+':
+            return operations["+"]
+    
+        default:
+            return operations[""]
+    }
+}
+
+
+
+export class Token {
+    operand: Operation
+    value: number
+    constructor(s: string) {   
+        const operandKey = s[0]
+        const operand: Operation = getOperandByKey(operandKey)
+        this.operand = operand;
+        this.value = operand.key === '' ? Number(s.substring(1)) : Number(s);
+    }
+}
+
+
 
 
   
