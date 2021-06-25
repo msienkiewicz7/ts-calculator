7f0eb182fa9d0eedb104b87ce0b1d33e74866288
msienkiewicz7 <msienkiewicz@disroot.org>
2021-06-25 18:47:10 +0200

    implement logical parts split

--- src/utils/ExpressionEvaluator/evaluator.ts
+++ src/utils/ExpressionEvaluator/evaluator.ts
@@ -1,25 +1,14 @@
 
-type Operation = {
-    key: string,
-    priority: number,
-    calculate: (a: number, b: number) => number
+export function evaluate(expression: string) : string {
+    //TODO: implement evaluation
+    return ''
 }
 
-export class ExpressionEvaluator {
-    add: Operation = {
-        key: "+",
-        priority: 0,
-        calculate: function (a: number, b: number): number {
-            return a + b
-        }
-    }
-
-    evaluate(expression: string) : string {
-        // 1. Find the highest priority operand from the left
-
-        // 2. Execute operation using adjacent numbers 
-        
-        return ""
-    }
-
+export function splitExpressionByLogic(expression: string) : string[] {
+    return expression.match(/[+\-*/]*(\.\d+|\d+(\.\d+)?)/g) || []  
 }
+
+
+
+  
+  
