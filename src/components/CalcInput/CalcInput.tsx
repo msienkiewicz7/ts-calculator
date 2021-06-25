fe1b3c7fa25d645c676924d7d2b82d3f921225f2
msienkiewicz7 <msienkiewicz@disroot.org>
2021-06-25 13:20:51 +0200

    make value prop optional and provide initial value

--- src/components/CalcInput/CalcInput.tsx
+++ src/components/CalcInput/CalcInput.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styles from './CalcInput.module.css';
 
 interface Props {
-  value: string
+  value?: string
 }
 
-const CalcInput = (props: Props) => (
+const CalcInput = ({value = ''}: Props) => (
   <div className={styles.CalcInput} data-testid="CalcInput">
-    <input disabled={true} value={props.value}/>
+    <input disabled={true} value={value}/>
   </div>
 );
 
